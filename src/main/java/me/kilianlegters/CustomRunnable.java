51331bb91b050217d34ae7738f3674adb06ee961
kilian544 <kilian.legters@gmail.com>
2019-07-16 17:28:01 +0200

    Added handling obstructed homes.

---
+++ src/main/java/me/kilianlegters/CustomRunnable.java
@@ -0,0 +1,38 @@
+package me.kilianlegters;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomRunnable extends BukkitRunnable {
+
+    private Homes homes;
+    private Player player;
+    private Location homeLocation;
+    private List<Location> queue;
+    private int iterations;
+
+    public CustomRunnable(Homes homes, Player player, Location homeLocation, List<Location> queue, int iterations){
+        this.homes = homes;
+        this.player = player;
+        this.homeLocation = homeLocation;
+        this.queue = queue;
+        this.iterations = iterations;
+    }
+
+    @Override
+    public void run() {
+        Location holder = this.homes.getSafeHomeLocation(homeLocation, queue, iterations);
+        if (holder != null){
+            this.homes.teleport(player, holder.setDirection(homeLocation.getDirection()));
+            this.homes.setHome(player, holder, true);
+            cancel();
+        }
+    }
+}
--- src/main/java/me/kilianlegters/Homes.java
+++ src/main/java/me/kilianlegters/Homes.java
@@ -1,6 +1,5 @@
 package me.kilianlegters;
 
-import com.oracle.deploy.update.UpdateCheck;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -13,8 +12,12 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.scheduler.BukkitRunnable;
 
-import javax.jnlp.DownloadService;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 import java.util.UUID;
 
 public class Homes implements CommandExecutor {
@@ -32,7 +35,7 @@ public class Homes implements CommandExecutor {
                 }
 
                 if (s.equalsIgnoreCase("home")) {
-                    return teleportHome(player);
+                    return tryTeleportHome(player);
                 }
 
                 if (s.equalsIgnoreCase("delhome")) {
@@ -65,22 +68,31 @@ public class Homes implements CommandExecutor {
         return true;
     }
 
-    private boolean teleportHome(Player player){
+    private boolean tryTeleportHome(Player player){
         if (!hasHome(player)){
             player.sendMessage(ChatColor.RED + "You do not yet have a home, use '/sethome'.");
             return false;
         }
-
-        Location homeLocation = getLocation(realLightWeightHomes.getConfig().getString(player.getUniqueId().toString());
-
+        Location homeLocation = getLocation(realLightWeightHomes.getConfig().getString(player.getUniqueId().toString()));
         if (!isHomeSafe(homeLocation)) {
-            makeHomeLocationSafe(homeLocation);
+            Location holder = getFastSafeHomeLocation(homeLocation);
+            if (holder == null) {
+                List<Location> queue = new ArrayList<>();
+                queue.add(homeLocation);
+                player.sendMessage(ChatColor.RED + "Your home is obstructed, looking for the nearest suitable alternative and bringing you there!");
+                new CustomRunnable(this, player, homeLocation, queue, 1000).runTaskTimer(realLightWeightHomes, 0, 1);
+                return true;
+            }
+            homeLocation = holder;
         }
+        teleport(player, homeLocation);
+        return true;
+    }
 
-        player.teleport(homeLocation);
+    public void teleport(Player player, Location location){
+        player.teleport(location.add(0.5, 0.2, 0.5));
         player.playSound(player.getLocation(), Sound.ENTITY_ENDERMAN_TELEPORT, SoundCategory.PLAYERS, 1, 1);
         player.sendMessage(ChatColor.GREEN + "Teleporting to home...");
-        return true;
     }
 
     private boolean setHome(Player player){
@@ -88,31 +100,95 @@ public class Homes implements CommandExecutor {
             player.sendMessage(ChatColor.RED + "You already have a home, use '/delhome' first.");
             return false;
         }
+        return setHome(player, player.getLocation(), false);
+    }
 
-        realLightWeightHomes.getConfig().set(player.getUniqueId().toString(), getString(player.getLocation()));
+    public boolean setHome(Player player, Location location, boolean forced){
+        realLightWeightHomes.getConfig().set(player.getUniqueId().toString(), getString(location));
         realLightWeightHomes.saveConfig();
-        player.sendMessage(ChatColor.GREEN + "Home set!");
+        if (forced){
+            player.sendMessage(ChatColor.GREEN + "Your home was automatically set!");
+        } else {
+            player.sendMessage(ChatColor.GREEN + "Home set!");
+        }
         return true;
     }
 
     private Location getLocation(String string){
         String[] strings = string.split("/");
-        return new Location(Bukkit.getWorld(UUID.fromString(strings[0])), Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), Integer.parseInt(strings[3]), Float.parseFloat(strings[4]), Float.parseFloat(strings[5])).add(.5, .2, .5);
+        return new Location(Bukkit.getWorld(UUID.fromString(strings[0])), Integer.parseInt(strings[1]), Integer.parseInt(strings[2]), Integer.parseInt(strings[3]), Float.parseFloat(strings[4]), Float.parseFloat(strings[5]));
     }
 
     private String getString(Location location){
         return location.getWorld().getUID().toString() + "/" + location.getBlockX() + "/" + location.getBlockY() + "/" + location.getBlockZ() + "/" + location.getYaw() + "/" + location.getPitch();
     }
 
-    private Boolean makeHomeLocationSafe(Location location){
-        return false;
+    private Boolean isHomeSafe(Location location){
+        return isHomeSafe(location.getBlock());
     }
 
-    private Boolean isHomeSafe(Location location){
-        Block lowerBlock = location.getBlock();
-        Block upperBlock = lowerBlock.getRelative(BlockFace.UP);
-        Block blockBeneath = lowerBlock.getRelative(BlockFace.DOWN);
-        return lowerBlock.isPassable() && lowerBlock.getType() != Material.LAVA && upperBlock.isPassable() && upperBlock.getType() != Material.LAVA;
+    private Boolean isHomeSafe(Block block){
+        Block upperBlock = block.getRelative(BlockFace.UP);
+        Block blockBeneath = block.getRelative(BlockFace.DOWN);
+        return block.isPassable() && block.getType() != Material.LAVA &&
+                upperBlock.isPassable() && upperBlock.getType() != Material.LAVA &&
+                !blockBeneath.isPassable() &&
+                block.getY() > 1;
+    }
+
+    private Location getFastSafeHomeLocation(Location location){
+        Block block = location.getBlock();
+        while (block.getRelative(BlockFace.DOWN).isPassable() && block.getY() > 0){
+            block = block.getRelative(BlockFace.DOWN);
+        }
+
+        if (isHomeSafe(block)){
+            return block.getLocation();
+        } else {
+            return null;
+        }
+    }
+
+    public Location getSafeHomeLocation(Location location, List<Location> queue, int iterations){
+        for (int i = 0; i < iterations; i++) {
+            Location locationToCheck = queue.remove(0);
+            if (locationToCheck.getBlock().isPassable()){
+                Location fastSafeHomeLocation = getFastSafeHomeLocation(locationToCheck);
+                if (fastSafeHomeLocation != null) {
+                    return fastSafeHomeLocation;
+                }
+            }
+            queue.addAll(getDivergingLocations(location, locationToCheck));
+        }
+        return null;
+    }
+
+    private List<Location> getDivergingLocations(Location origin, Location check){
+        int accuracy = 5;
+        List<Location> holder = new ArrayList<>();
+        int xSign = Integer.signum(check.getBlockX() - origin.getBlockX()),
+                ySign = Integer.signum(check.getBlockY() - origin.getBlockY()),
+                zSign = Integer.signum(check.getBlockZ() - origin.getBlockZ());
+
+        if (xSign != 0){
+            holder.add(check.clone().add(xSign * accuracy, 0,0));
+        } else {
+            holder.add(check.clone().add(-1 * accuracy, 0, 0));
+            holder.add(check.clone().add(1 * accuracy, 0, 0));
+        }
+        if (ySign != 0){
+            holder.add(check.clone().add(0, ySign * accuracy, 0));
+        } else {
+            holder.add(check.clone().add(0, -1 * accuracy, 0));
+            holder.add(check.clone().add(0, 1 * accuracy, 0));
+        }
+        if (zSign != 0){
+            holder.add(check.clone().add(0, 0, zSign * accuracy));
+        } else {
+            holder.add(check.clone().add(0, 0, -1 * accuracy));
+            holder.add(check.clone().add(0, 0, 1 * accuracy));
+        }
+        return holder;
     }
 
 }
